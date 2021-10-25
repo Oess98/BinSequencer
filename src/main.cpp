cba7a67339e918ab20ec7e7a78b2d5bcd5316f01
Oess98 <oess-98@hotmail.com>
2021-10-25 17:36:03 +0200

    Pointer functions New functions with pointers to replace old code
    New functions:
    unsignedByteToBinaryPins
    signedByteToBinaryPins
    setPinsMode

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,36 @@
 const uint8_t A []= {2,3,4,5};
 const uint8_t B []= {6,7,8};
 
-const uint8_t arrPointer[]={*A, *B};
 
-void unsignedByteToBinaryPins(uint8_t dec, uint8_t *pins, size_t size)
+void setPinsToMode(const uint8_t* array, size_t size,const uint8_t mode)
 {
-  for (uint8_t bitNumber = 0; bitNumber<size; bitNumber++)
+  for(uint8_t i {0}; i<size; i++)
+  {
+    pinMode(array[i], mode);
+  }
+}
+
+void unsignedByteToBinaryPins(const uint8_t dec, const uint8_t *pins, size_t size)
+{
+  for (uint8_t bitNumber {0}; bitNumber<size; bitNumber++)
   {
     digitalWrite(pins[bitNumber], bitRead(dec, bitNumber));
   }
 }
-void signedByteToBinaryPins(int8_t dec, uint8_t *pins, size_t size)
+void signedByteToBinaryPins(const int8_t dec, const uint8_t *pins, size_t size)
 {
-  for (uint8_t bitNumber = 0; bitNumber<size; bitNumber++)
+  for (uint8_t bitNumber {0}; bitNumber<size; bitNumber++)
   {
     digitalWrite(pins[bitNumber], bitRead(dec, bitNumber));
   }
 }
+
 
 void setup() 
 {
   Serial.begin(115200);
-  for(uint8_t pin: A)
-  {
-    pinMode(pin, OUTPUT);
-  }
-  for(uint8_t pin: B)
-  {
-    pinMode(pin, OUTPUT);
-  }
+  setPinsToMode(A, sizeof(A),OUTPUT);
+  setPinsToMode(B,sizeof(B), OUTPUT);
 }
 
 void loop() 
